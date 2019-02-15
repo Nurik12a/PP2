6b9725f0cea21a4b827ffaa6e7162813bcb83192
Nursultan <nurtuganulyn@gmail.com>
2019-02-15 12:00:25 +0600

    modifying

--- Week3/Task1/Program.cs
+++ Week3/Task1/Program.cs
@@ -5,125 +5,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Example3
+namespace FarManager
 {
-    class Layer
+    enum FSIMode
     {
-        public int selectedItem;
-        public FileSystemInfo[] items;
-
-        public Layer(FileSystemInfo[] items)
-        {
-            selectedItem = 0;
-            this.items = items;
-        }
-        
-
-        public void Draw(bool mode)
-        {
-            
-            if (mode == true)
-            {
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.Clear();
-                
-                for (int i = 0; i < items.Length; ++i)
-                {
-                    
-                    if (i == selectedItem)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                    }
-                    else
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black;
-                    }
-                    if (items[i].GetType() == typeof(DirectoryInfo))
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                    }
-                    Console.WriteLine(items[i].Name);
-                }
-            }
-            else
-            {
-
-            }
-        }
+        DirectoryInfo = 1,
+        File = 2
     }
 
+    class Layer
+    {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+        public int SelectedIndex
+        {
+            get;
+            set;
+        }
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if (i == SelectedIndex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow ;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                if (Content[i].GetType() == typeof(FileInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine((i + 1) + ". " + Content[i].Name);
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\Nurik\Desktop\cart-master");
-            Stack<Layer> history = new Stack<Layer>();
-            history.Push(new Layer(dirInfo.GetFileSystemInfos()));
-
-            bool quit = false;
-
-            bool mode = true;//1 - folder, 0 - file
-
-            while (!quit)
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Nurik\Desktop\Eshopper");
+            Layer l = new Layer
             {
-                history.Peek().Draw(mode);
+                Content = dir.GetFileSystemInfos(),
+                SelectedIndex = 0
+            };
+
+
+            FSIMode curMode = FSIMode.DirectoryInfo;
+
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(l);
+
+            bool esc = false;
+            while (!esc)
+            {
+                history.Peek().Draw();
                 ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
                 switch (consoleKeyInfo.Key)
                 {
-                    case ConsoleKey.UpArrow:
-                        if (history.Peek().selectedItem - 1 < 0)
+                    case ConsoleKey.UpArrow: //Up
+                        if (history.Peek().SelectedIndex - 1 < 0)
                         {
-                            history.Peek().selectedItem = history.Peek().items.Length - 1;
+                            history.Peek().SelectedIndex = history.Peek().Content.Length - 1;
                         }
                         else
                         {
-                            history.Peek().selectedItem--;
+                            history.Peek().SelectedIndex--;
                         }
                         break;
-                    case ConsoleKey.DownArrow:
-                        if (history.Peek().selectedItem + 1 >= history.Peek().items.Length)
+
+                    case ConsoleKey.DownArrow: //Down
+                        if (history.Peek().SelectedIndex+1 >= history.Peek().Content.Length )
                         {
-                            history.Peek().selectedItem = 0;
+                            history.Peek().SelectedIndex = 0;
                         }
                         else
                         {
-                            history.Peek().selectedItem++;
+                            history.Peek().SelectedIndex++;
                         }
                         break;
-                    case ConsoleKey.Enter:
-                        int x = history.Peek().selectedItem;
-                        FileSystemInfo selectedFSI = history.Peek().items[x];
-                        if (selectedFSI.GetType() == typeof(DirectoryInfo))
+
+                    case ConsoleKey.Enter: //Inside
+                        int index = history.Peek().SelectedIndex;
+                        FileSystemInfo fsi = history.Peek().Content[index];
+                        if (fsi.GetType() == typeof(DirectoryInfo))
                         {
-                            FileSystemInfo[] items = (selectedFSI as DirectoryInfo).GetFileSystemInfos();
-                            history.Push(new Layer(items));
+                            curMode = FSIMode.DirectoryInfo;
+                            DirectoryInfo d = fsi as DirectoryInfo;
+                            history.Push(new Layer
+                            {
+                                Content = d.GetFileSystemInfos(),
+                                SelectedIndex = 0
+                            });
                         }
                         else
                         {
-                            mode = false;
-                            FileStream fs = new FileStream(selectedFSI.FullName, FileMode.Open, FileAccess.Read);
-                            StreamReader sr = new StreamReader(fs);
-
-                            Console.BackgroundColor = ConsoleColor.White;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            Console.Clear();
-                            Console.WriteLine(sr.ReadToEnd());
-
-                            sr.Close();
-                            fs.Close();
+                            curMode = FSIMode.File;
+                            using (FileStream fs = new FileStream(fsi.FullName, FileMode.Open, FileAccess.Read))
+                            {
+                                using (StreamReader sr = new StreamReader(fs))
+                                {
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Clear();
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
                         }
-
                         break;
-                    case ConsoleKey.Backspace:
-                        mode = true;
+
+                    case ConsoleKey.Backspace: //Back
+                        if (curMode == FSIMode.DirectoryInfo)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            curMode = FSIMode.DirectoryInfo;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+
+                    case ConsoleKey.Delete: //Remove
+                        int iter = history.Peek().SelectedIndex;
+                        FileSystemInfo fsi1 = history.Peek().Content[iter];
+                        Console.Clear();
+                        Console.WriteLine("Do you want to delete {0} ?\nPress [Y/N]", fsi1.Name);
+                        ConsoleKeyInfo res = Console.ReadKey();
+                        while (res.Key != ConsoleKey.Y && res.Key != ConsoleKey.N)
+                            res = Console.ReadKey();
+                        if (res.Key == ConsoleKey.Y)
+                        {
+                            fsi1.Delete();
+                        }
+                        else { break; }
                         history.Pop();
                         break;
-                    case ConsoleKey.Escape:
-                        quit = true;
+
+                    case ConsoleKey.F: //Rename
+                        int iter1 = history.Peek().SelectedIndex;
+                        FileSystemInfo fsi2 = history.Peek().Content[iter1];
+                        Console.WriteLine("Enter new name:");
+                        string name = Console.ReadLine();
+                        string prev = fsi2.FullName;
+
+                        string newName = Path.Combine(Path.GetDirectoryName(prev), name);
+                        Directory.Move(prev, newName);
+                        history.Pop();
+                        break;
+
+                    case ConsoleKey.Escape: //Exit
+                        esc = true;
                         break;
                 }
             }
--- Week3/Task1/Properties/AssemblyInfo.cs
+++ Week3/Task1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Task3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Task3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("4432dd24-d849-4c8c-ae35-b5818029ee92")]
+[assembly: Guid("e447811d-5856-4aaa-9377-50c2758bd445")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
